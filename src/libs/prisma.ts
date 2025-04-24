4f66af9b243ac3c1887e250534cace738899dd2a
GabrielRCodes <gabrielrcodes@gmail.com>
2025-04-24 15:08:35 -0300

    :sparkles: feat(prisma): creating the prisma client to interact with the database

---
+++ src/libs/prisma.ts
@@ -0,0 +1,16 @@
+import { PrismaClient } from '@prisma/client';
+
+// PrismaClient é vinculado a uma variável global em desenvolvimento para evitar
+// múltiplas instâncias durante hot-reloading.
+declare global {
+  // eslint-disable-next-line no-var
+  var prisma: PrismaClient | undefined;
+}
+
+export const prisma =
+  global.prisma ||
+  new PrismaClient({
+    log: ['query', 'error', 'warn'],
+  });
+
+if (process.env.NODE_ENV !== 'production') global.prisma = prisma; 
